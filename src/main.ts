979c5e6b7ba1bb00bde2447b4924e2712ef010ca
Lian <ln.studer@protonmail.ch>
2022-05-17 08:38:29 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://talentpool-web.herokuapp.com'],
+    origin: ['http://localhost:3000', 'https://talentpool.tk'],
     credentials: true,
   });
 
